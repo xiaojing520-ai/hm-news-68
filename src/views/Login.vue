db15300ac8ef71da3b6963a75423cf7b61d60745
xiaojing520-ai <542258713@qq.com>
2020-08-26 21:54:41 +0800

    配置项目中的vant

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="login">登录组件</div>
+  <div class="login">
+    登录组件
+    <span class="iconfont iconweixin"></span>
+    <span class="iconfont icondianzan"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
